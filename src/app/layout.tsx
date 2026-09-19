c506d78953633a817a4fcbcb5763b64c921cfdc3
Devvratbytelogic <support@bytelogicindia.com>
2026-09-19 14:57:39 +0530

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const revalidate = 3600;
+
 export const metadata: Metadata = {
   title: "Goodies",
   description: "A simple Next.js project with Tailwind CSS",
